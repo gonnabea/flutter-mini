fc5fa614572093704481060b7d86d3bba6359463
gonnabea <winning1993@naver.com>
2023-01-04 22:27:31 +0900

    [ADD] #3.0 Header

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
+class Player {
+  String? name;
+
+  Player({required this.name});
+}
+
 void main() {
+  var jiwon = Player(name: "jiwon");
+  print(jiwon);
   runApp(App());
 }
 
@@ -9,8 +17,37 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Hello flutter!')),
-        body: Center(child: Text('Hello world!')),
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  children: [
+                    Text('Hey, Selena',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 48,
+                            fontWeight: FontWeight.w700)),
+                    Text('Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(1),
+                          fontSize: 18
+                        )
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),),
       ),
     );
   }
