05401f93ae9b96c9cd16f9a3bf1221878910e98f
gonnabea <winning1993@naver.com>
2023-01-05 22:45:43 +0900

    [ADD] #3.2 Buttons Section

--- lib/main.dart
+++ lib/main.dart
@@ -21,33 +21,77 @@ class App extends StatelessWidget {
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40),
           child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            SizedBox(
-              height: 80,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Column(
-                  children: [
-                    Text('Hey, Selena',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 48,
-                            fontWeight: FontWeight.w700)),
-                    Text('Welcome back',
-                        style: TextStyle(
-                          color: Color.fromRGBO(255, 255, 255, 0.8),
-                          fontSize: 18
-                        )
-                    ),
-                  ],
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    children: [
+                      Text('Hey, Selena',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 48,
+                              fontWeight: FontWeight.w700)),
+                      Text('Welcome back',
+                          style: TextStyle(
+                              color: Color.fromRGBO(255, 255, 255, 0.8),
+                              fontSize: 18)),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
                 )
-              ],
-            )
-          ],
-        ),),
+              ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  '\$5 194 482',
+                  style: TextStyle(
+                    fontSize: 42,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white.withOpacity(0.8),
+                )
+              ),
+              SizedBox(height: 20,),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45)
+                    ),
+                    child: 
+                    Padding(padding: EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 50,
+                    ),
+                    child: Text(
+                      'Transfer',
+                      style: TextStyle(
+                        fontSize: 20,
+                      )
+                    ),
+                    )
+                    
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
